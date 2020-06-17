4fb5f272d1b6ceda76a3c844de87f63be8987d58
Julie Greene <julie_greene22@yahoo.com>
2020-06-17 10:01:51 -0600

    added solar systems component and link to navbar

---
+++ client/src/screens/solarSystems/SolarSystem.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from "react";
+
+const SolarSystems = (props) => {
+  
+  //componentDidMount 
+  //do api calls in here once setup 
+  useEffect(() => {
+    console.log("mounted");
+  }, []);
+
+  return (
+    <div>
+      <h1>Solar Systems</h1>
+    </div>
+  )
+};
+
+export default SolarSystems; 
